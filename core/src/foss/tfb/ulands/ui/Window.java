b643bf68d2e9d92252e5410acce1896055bfb163
icydev <icydevelop@protonmail.com>
2022-01-20 16:59:45 +0300

    add SettingsScreen Template

--- core/src/foss/tfb/ulands/ui/Window.java
+++ core/src/foss/tfb/ulands/ui/Window.java
@@ -3,27 +3,32 @@ package foss.tfb.ulands.ui;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class Window extends com.badlogic.gdx.scenes.scene2d.ui.Window {
-    public Window(String title, Skin skin) {
+    public Window(String title, Skin skin)
+    {
         super(title, skin);
         init();
     }
 
-    public Window(String title, Skin skin, String styleName) {
+    public Window(String title, Skin skin, String styleName)
+    {
         super(title, skin, styleName);
         init();
     }
 
-    public Window(String title, WindowStyle style) {
+    public Window(String title, WindowStyle style)
+    {
         super(title, style);
         init();
     }
 
-    private void init(){
+    private void init()
+    {
 //        this.pad(30);
         this.setMovable(true);
         this.setResizable(true);
         this.pad(30);
         this.padTop(60);
+
 //        this.set
     }
 
