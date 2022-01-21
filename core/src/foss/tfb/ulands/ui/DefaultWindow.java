5f1f7848113ee68d4bf8ccac9a291ade5c4bf698
Yankovskiy Georgiy <admin@yageorgiy.ru>
2022-01-22 01:17:34 +0300

    Add window actions button (test), UI widgets: scroll, list-selection

--- core/src/foss/tfb/ulands/ui/DefaultWindow.java
+++ core/src/foss/tfb/ulands/ui/DefaultWindow.java
@@ -1,21 +1,29 @@
 package foss.tfb.ulands.ui;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 public class DefaultWindow extends Window {
 
     public static final String CLOSE_BUTTON_STYLE = "close-button";
     public static final String ENLARGE_BUTTON_STYLE = "enlarge-button";
+    public static final String SUBMENU_BUTTON_STYLE = "actions-list-style";
 
     protected boolean closeButtonEnabled = true;
     protected boolean enlargeButtonEnabled = true;
+
+    public boolean intendedToBeEnlarged = false;
+    public float lastW, lastH = 0;
+    public float lastX, lastY = 0;
 
     public DefaultWindow(String title, Skin skin) {
         super(title, skin);
@@ -42,6 +50,7 @@ public class DefaultWindow extends Window {
         this.padTop(48);
         this.setRotation(0);
 
+        this.subMenuButton();
         this.enlargeButton();
         this.closeButton();
 
@@ -67,19 +76,17 @@ public class DefaultWindow extends Window {
                 .padTop(5);
     }
 
+
+
+
     public void enlargeButton()
     {
         if(!enlargeButtonEnabled) return;
 
         final Button enlargeButton = new Button(this.getSkin(), DefaultWindow.ENLARGE_BUTTON_STYLE);
+
         // TODO: enlarge button animation
         enlargeButton.addListener(new ClickListener() {
-
-            // TODO: move to DefaultWindow class
-            private boolean intendedToBeEnlarged = false;
-            private float lastW, lastH = 0;
-            private float lastX, lastY = 0;
-
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Stage stage = getStage();
@@ -90,16 +97,56 @@ public class DefaultWindow extends Window {
                     lastX = getX();
                     lastY = getY();
                     setBounds(0, 0, stage.getWidth(),stage.getHeight());
+                    intendedToBeEnlarged = !intendedToBeEnlarged;
                 } else {
                     setBounds(lastX, lastY, lastW, lastH);
+                    // intendedToBeEnlarged will already be false as size would change
                 }
 
-                intendedToBeEnlarged = !intendedToBeEnlarged;
             }
         });
 
         getTitleTable()
                 .add(enlargeButton)
+                .size(32, 32)
+                .padRight(10)
+                .padTop(5);
+    }
+
+    public void subMenuButton()
+    {
+        if(!enlargeButtonEnabled) return;
+
+        // TODO: make dedicated ActionsListBox class
+        final SelectBox<String> subMenuButton = new SelectBox<String>(this.getSkin(), DefaultWindow.SUBMENU_BUTTON_STYLE){
+            @Override
+            public void draw (Batch batch, float parentAlpha) {
+                validate();
+
+                Drawable background = getBackgroundDrawable();
+
+                Color color = getColor();
+                float x = getX(), y = getY();
+                float width = getWidth(), height = getHeight();
+
+                batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
+                if (background != null)
+                    background.draw(batch, x, y, width, height);
+
+            }
+        };
+
+        subMenuButton.getList().addListener(new ClickListener() {
+            public void clicked (InputEvent event, float x, float y) {
+                // TODO: fix input lag
+                System.out.println(subMenuButton.getSelected());
+            }
+        });
+
+        subMenuButton.setItems("item0", "item1", "item2");
+
+        getTitleTable()
+                .add(subMenuButton)
                 .size(32, 32)
                 .padRight(10)
                 .padTop(5);
@@ -117,6 +164,13 @@ public class DefaultWindow extends Window {
 //        shapeRenderer.rect(this.getX(), this.getY(), width, height);
 //        shapeRenderer.
 //        shapeRenderer.end();
+    }
+
+    @Override
+    protected void sizeChanged()
+    {
+        super.sizeChanged();
+        intendedToBeEnlarged = false;
     }
 
     public boolean isCloseButtonEnabled()
