c80ebf5a58491c57ff2e946bd6874976c270aa9d
Yankovskiy Georgiy <admin@yageorgiy.ru>
2022-02-03 14:24:54 +0300

    Desktop application icon logo

--- desktop/src/foss/tfb/ulands/desktop/DesktopLauncher.java
+++ desktop/src/foss/tfb/ulands/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package foss.tfb.ulands.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import foss.tfb.ulands.UlandsTFBGame;
@@ -8,6 +9,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Ultimate Lands: The Forced Behaviour";
+		config.addIcon("desktop-launcher-logo-128.png", Files.FileType.Internal);
+		config.addIcon("desktop-launcher-logo-32.png", Files.FileType.Internal);
+		config.addIcon("desktop-launcher-logo-16.png", Files.FileType.Internal);
 		new LwjglApplication(new UlandsTFBGame(), config);
 	}
 }
